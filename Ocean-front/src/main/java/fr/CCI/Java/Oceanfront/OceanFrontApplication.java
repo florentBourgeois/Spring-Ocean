7fa756b108d6a1ec32c2c639bb77605b0aa0b114
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-07-11 06:24:13 +0200

    Ajout du projet Ocean-Front

---
+++ Ocean-front/src/main/java/fr/CCI/Java/Oceanfront/OceanFrontApplication.java
@@ -0,0 +1,13 @@
+package fr.CCI.Java.Oceanfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanFrontApplication.class, args);
+	}
+
+}
